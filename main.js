d4f5099d5be2ed12542485467a98978da7fc441b
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-06-03 13:08:17 -0700

    Create functions that apply the appropriate CSS styling in response to which arrow key was pressed. #2

--- main.js
+++ main.js
@@ -1,4 +1,41 @@
-document.addEventListener('keydown', turnCar);
+var $car = document.querySelector('img');
 
+document.addEventListener('keydown', turnCar);
 function turnCar(event) {
+  turnRight();
+  turnLeft();
+  turnUp();
+  turnDown();
+}
+
+function turnRight() {
+  var ArrowRight = 39;
+  if (event.which !== ArrowRight) {
+    return;
+  }
+  $car.className = '';
+}
+
+function turnLeft() {
+  var ArrowLeft = 37;
+  if (event.which !== ArrowLeft) {
+    return;
+  }
+  $car.className = 'face-left';
+}
+
+function turnUp() {
+  var ArrowUp = 38;
+  if (event.which !== ArrowUp) {
+    return;
+  }
+  $car.className = 'face-up';
+}
+
+function turnDown() {
+  var ArrowDown = 40;
+  if (event.which !== ArrowDown) {
+    return;
+  }
+  $car.className = 'face-down';
 }
